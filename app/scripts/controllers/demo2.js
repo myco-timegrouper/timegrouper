956c32173a55d5aaad61f86f32e5bf6993b4f07c
intuinno <intuinno@gmail.com>
2015-04-26 09:47:06 -0400

    CORS error
    ToDo
    - Rainbow color scale
    - Using dummy, develop hierarchical sim matrix

--- app/scripts/controllers/demo2.js
+++ app/scripts/controllers/demo2.js
@@ -8,40 +8,52 @@
  * Controller of the timegrouperApp
  */
 angular.module('timegrouperApp')
-  .controller('Demo2Ctrl', function ($scope) {
+  .controller('Demo2Ctrl', function ($scope,$http) {
 
         var loadData = function() {
 
-            d3.text('data/SimMatLarge.csv', function(text) {
-                var data = d3.csv.parseRows(text).map(function(row, i) {
-                    return row.map(function(value, j) {
-                        return {
-                            x: j,
-                            y: i,
-                            z: +value
-                        };
-                    });
-                });
+            var url = 'https://lit-hollows-6344.herokuapp.com/getSimMatrix';
 
-                $scope.simMatrix = data;
-
-                d3.text('data/PatchNameLarge.csv', function(text) {
-                    var names = d3.csv.parseRows(text);
-
-                    var namesList = names.map(function(d, i) {
-                        return {
-                            name: d[0],
-                            count: 0,
-                            group: 1,
-                            index: i
-                        };
-                    });
-
-                    $scope.orderList = namesList;
-
-                    $scope.$apply();
-                });
+            $http.post(url, {simMetric:'nmf_euc', cAlgorithm:'kmeans', appName:'chrome'}).success( function(data, status, headers, config) {
+                console.log(data);
+            }).error(function(data,status,headers, config) {
+                console.log(status);
             });
+
+
+
+
+
+            // d3.text('data/SimMatLarge.csv', function(text) {
+            //     var data = d3.csv.parseRows(text).map(function(row, i) {
+            //         return row.map(function(value, j) {
+            //             return {
+            //                 x: j,
+            //                 y: i,
+            //                 z: +value
+            //             };
+            //         });
+            //     });
+
+            //     $scope.simMatrix = data;
+
+            //     d3.text('data/PatchNameLarge.csv', function(text) {
+            //         var names = d3.csv.parseRows(text);
+
+            //         var namesList = names.map(function(d, i) {
+            //             return {
+            //                 name: d[0],
+            //                 count: 0,
+            //                 group: 1,
+            //                 index: i
+            //             };
+            //         });
+
+            //         $scope.orderList = namesList;
+
+            //         $scope.$apply();
+            //     });
+            // });
 
         };
 
